d23ad697c350aa551de26758f08125658ec3fbb2
Nik <nerdvestor@gmail.com>
2020-11-14 16:15:04 -0800

    add routing

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,8 @@
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client"
 import React from "react"
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import "./App.css"
+import Landing from "./components/Landing"
 import Users from "./components/Users"
 
 const client = new ApolloClient({
@@ -11,9 +13,16 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div>
-        <Users />
-      </div>
+      <Router>
+        <Switch>
+          <Route path="/landing" >
+            <Landing />
+          </Route>
+          <Route path="/">
+            <Users />
+          </Route>
+        </Switch>
+      </Router>
     </ApolloProvider>
   )
 }
---
+++ web/src/components/Landing.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Landing() {
+    return (
+        <div>
+            <h1>Landing</h1>
+        </div>
+    )
+}
