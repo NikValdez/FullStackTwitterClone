fc33ba5deafd023e1a06b0bc4c90113ff287d87e
Nik <nerdvestor@gmail.com>
2020-11-30 18:08:46 -0800

    style landing page video 8

--- web/src/components/Landing.tsx
+++ web/src/components/Landing.tsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React from "react"
+import { Link } from "react-router-dom"
+import TwitterLogo from "../styles/assets/twitter-logo.png"
+import "../styles/landing.css"
 
-export default function Landing() {
-    return (
-        <div>
-            <h1>Landing</h1>
+function Landing() {
+  return (
+    <div className="main">
+      <div className="wrapper">
+        <div className="left">
+          <div className="items-wrapper">
+            <div className="item">
+              <span className="icon">
+                <i className="fa fa-search" aria-hidden="true"></i>
+              </span>
+              <span className="label">Follow your interests.</span>
+            </div>
+            <div className="item">
+              <span className="icon">
+                <i className="fa fa-user" aria-hidden="true"></i>
+              </span>
+              <span className="label">Hear what people are talking about.</span>
+            </div>
+            <div className="item">
+              <span className="icon">
+                <i className="fa fa-comment" aria-hidden="true"></i>
+              </span>
+              <span className="label">Join the conversation.</span>
+            </div>
+          </div>
         </div>
-    )
+
+        <div className="center">
+          <img src={TwitterLogo} alt="logo" style={{ width: "50px" }} />
+          <h1>
+            See what's happening in
+            <br />
+            the world right now
+          </h1>
+          <span>Join Twitter Today.</span>
+          <Link to="/signup" className="btn-sign-up">
+            Sign up
+          </Link>
+          <Link to="/login" className="btn-login">
+            Log in
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
 }
+
+export default Landing
--- web/src/pages/Signup.tsx
+++ web/src/pages/Signup.tsx
@@ -1,95 +1,98 @@
 import { gql, useMutation } from "@apollo/client"
-import { ErrorMessage, Field, Form, Formik } from 'formik'
+import { ErrorMessage, Field, Form, Formik } from "formik"
 import React from "react"
 import { Link, useHistory } from "react-router-dom"
-import * as Yup from 'yup'
-import TwitterLogo from '../styles/assets/twitter-logo.png'
+import * as Yup from "yup"
+import TwitterLogo from "../styles/assets/twitter-logo.png"
 
 const SIGNUP_MUTATION = gql`
-mutation signup($name: String, $email: String!, $password: String!) {
+  mutation signup($name: String, $email: String!, $password: String!) {
     signup(name: $name, email: $email, password: $password) {
-        token
+      token
     }
-}
+  }
 `
 
 interface SignupValues {
-    email: string
-    password: string
-    confirmPassword: string
-    name: string
+  email: string
+  password: string
+  confirmPassword: string
+  name: string
 }
 
 function Signup() {
-    const history = useHistory()
-    const [signup, { data }] = useMutation(SIGNUP_MUTATION)
+  const history = useHistory()
+  const [signup, { data }] = useMutation(SIGNUP_MUTATION)
 
-    const initialValues: SignupValues = {
-        email: '',
-        password: '',
-        confirmPassword: '',
-        name: ''
-    }
+  const initialValues: SignupValues = {
+    email: "",
+    password: "",
+    confirmPassword: "",
+    name: "",
+  }
 
+  const validationSchema = Yup.object({
+    email: Yup.string()
+      .email("Invalid email address")
+      .required("Email Required"),
+    password: Yup.string()
+      .max(20, "Must be 20 characters or less")
+      .required("Password Required"),
+    confirmPassword: Yup.string().oneOf(
+      [Yup.ref("password")],
+      "Passwords must match"
+    ),
+    name: Yup.string()
+      .max(15, "Must be 15 characters or less")
+      .required("Name Required"),
+  })
 
-    const validationSchema = Yup.object({
-        email: Yup.string()
-            .email("Invalid email address")
-            .required("Email Required"),
-        password: Yup.string()
-            .max(20, "Must be 20 characters or less")
-            .required("Password Required"),
-        confirmPassword: Yup.string().oneOf(
-            [Yup.ref("password")],
-            "Passwords must match"
-        ),
-        name: Yup.string()
-            .max(15, "Must be 15 characters or less")
-            .required("Name Required"),
-
-    })
-
-    return (
-        <div>
-            <img src={TwitterLogo}
-                alt='logo'
-                style={{ width: "50px" }}
-                className="logo"
-            />
-            <h3>Sign up</h3>
-            <Formik
-                initialValues={initialValues}
-                validationSchema={validationSchema}
-                onSubmit={async (values, { setSubmitting }) => {
-                    setSubmitting(true)
-                    const response = await signup({
-                        variables: values
-                    })
-                    localStorage.setItem("token", response.data.signup.token)
-                    setSubmitting(false)
-                    history.push('/users')
-                }}
-            >
-                <Form>
-                    <Field name="email" type="text" placeholder="Email" />
-                    <ErrorMessage name="email" component={'div'} />
-                    <Field name="name" type="text" placeholder="Name" />
-                    <ErrorMessage name="name" component={'div'} />
-                    <Field name="password" type="password" placeholder="Password" />
-                    <ErrorMessage name="password" component={'div'} />
-                    <Field name="confirmPassword" type="password" placeholder="Confirm Password" />
-                    <ErrorMessage name="confirmPassword" component={'div'} />
-                    <button type="submit" className="login-button"><span>Sign up</span></button>
-                </Form>
-
-            </Formik>
-            <div className="register">
-                <h4>Already have an account?</h4>
-                <Link to="/login" >Log in</Link>
-            </div>
-        </div>
-    )
+  return (
+    <div className="container">
+      <img
+        src={TwitterLogo}
+        alt="logo"
+        style={{ width: "50px" }}
+        className="logo"
+      />
+      <h3>Sign up</h3>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={async (values, { setSubmitting }) => {
+          setSubmitting(true)
+          const response = await signup({
+            variables: values,
+          })
+          localStorage.setItem("token", response.data.signup.token)
+          setSubmitting(false)
+          history.push("/users")
+        }}
+      >
+        <Form>
+          <Field name="email" type="text" placeholder="Email" />
+          <ErrorMessage name="email" component={"div"} />
+          <Field name="name" type="text" placeholder="Name" />
+          <ErrorMessage name="name" component={"div"} />
+          <Field name="password" type="password" placeholder="Password" />
+          <ErrorMessage name="password" component={"div"} />
+          <Field
+            name="confirmPassword"
+            type="password"
+            placeholder="Confirm Password"
+          />
+          <ErrorMessage name="confirmPassword" component={"div"} />
+          <button type="submit" className="login-button">
+            <span>Sign up</span>
+          </button>
+        </Form>
+      </Formik>
+      <div className="register">
+        <h4>Already have an account?</h4>
+        <Link to="/login">Log in</Link>
+      </div>
+    </div>
+  )
 }
-
 
 export default Signup
